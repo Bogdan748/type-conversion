144f5d0bae763c43661a3c89131ffe746d11e147
Bogdan748 <andriesbogdan00@gmail.com>
2022-03-14 20:03:48 +0200

    Sample with iperators overloading

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[X=10,Y=20]
+            //Conversion
+
+            
+
+            Point p = new Point
+            {
+                X = 10,
+                Y = 20
+            };
+
+            string pointAsText = p;
+            Console.WriteLine($"Conversie implicita: {pointAsText}");
+
+
+            int i = 10;
+            i.ToString();
+            string a = "10";
+            int.TryParse(a, out int result);
+
+            Console.WriteLine(p.ToString());
+
+            string someOtherPoint = "[X=50,Y=90]";
+            if (Point.TryParse(someOtherPoint, out Point point))
+            {
+                Console.WriteLine(point.ToString());
+               
+            }
+            else
+            {
+                Console.WriteLine("nu mere");
+            }
+
+
+            Point otherPoint = (Point)"[X=30,Y=30]";
+            Console.WriteLine(otherPoint);
+
+            Point pointSum = p + otherPoint;
+            Console.WriteLine(pointSum.ToString());
+        }
+    }
+}
